19746a0237cac33a5be8317de9655dca0b01633f
Valdir2373 <valdirdesouzajunioradm@gmail.com>
2026-06-18 16:37:21 -0300

    subindo projeto fdp

---
+++ src/infra/ExpressServerAdapter.ts
@@ -0,0 +1,21 @@
+import { methodHttp, middleWare, ServerPort } from "./ServerPort";
+import express, { Express } from "express"
+
+export class ExpressServerAdapter extends ServerPort {
+    private app:Express
+    constructor(){
+        super()
+        this.app = express()
+        this.app.use(express.json({limit:"100mb"}))
+    }
+
+    addRouter(method: methodHttp, path: string, callback: middleWare): void {
+        console.log(`Rota ${method.toUpperCase()}: ${path}`);
+        this.app[method](path,callback)
+    }
+
+    listen(port: number): void {
+        this.app.listen(port, ()=> console.log(port))
+    }
+
+}
---
+++ src/infra/ServerPort.ts
@@ -0,0 +1,19 @@
+export type methodHttp = "get"|"post"|"put"|"delete"
+
+export interface IReq {
+    body:any,
+}
+
+
+export interface IRes {
+    send(message:string):void,
+    json(body:any):void,
+    status(sts:number):IRes,
+}
+
+export type middleWare = (req:IReq,res:IRes) => void
+
+export abstract class ServerPort {
+    addRouter(method:methodHttp, path:string, callback:middleWare){}
+    listen(port:number){}
+}
